fcbf551db48d1cf8a255eb0923df4e7c0ba9e81b
dlsrks <paulhjkim3@gmail.com>
2023-11-02 11:38:04 +0900

    번역/리셋 기능 완성

--- googlt_translate_app.py
+++ googlt_translate_app.py
@@ -16,10 +16,10 @@ class GoogleTranslate(QMainWindow, form_class):
         self.setupUi(self)  # 제작해놓은 UI 연결
         self.setWindowTitle('구글 번역기')  # 번역기 앱 타이틀
         self.setWindowIcon(QIcon('D:/KimHyunJae/자료/아이콘/google_icon.png'))  # 앱 아이콘
-        self.statusBar().showMessage('Google Transe App v1.0_Copyright by dlsrks-437')
+        self.statusBar().showMessage('Google Transe App v1.0_Copyright ⓒ by dlsrks-437')
 
         self.btn_trans.clicked.connect(self.trans_function)
-        self.reset_btn.clicked.connect(self.reset())
+        self.reset_btn.clicked.connect(self.reset)
 
     def trans_function(self):
         trans_kor = self.input_kor_text.text()  # input_kor_text에 입력된 한글 가져오기
@@ -37,6 +37,9 @@ class GoogleTranslate(QMainWindow, form_class):
 
     def reset(self):
         self.input_kor_text.clear()
+        self.output_eng_text.clear()
+        self.output_jap_text.clear()
+        self.output_chn_text.clear()
 
 
 if __name__ == '__main__':
